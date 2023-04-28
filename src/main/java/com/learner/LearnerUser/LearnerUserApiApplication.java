97ea411d09143b23b7ff9a5eb9f2870712cb778b
skandh.15 <skandhoff@gmail.com>
2023-04-28 11:45:19 +0530

    indentation of code.

--- src/main/java/com/learner/LearnerUser/LearnerUserApiApplication.java
+++ src/main/java/com/learner/LearnerUser/LearnerUserApiApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class LearnerUserApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnerUserApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearnerUserApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/learner/LearnerUser/exception/GlobalExceptionHandler.java
+++ src/main/java/com/learner/LearnerUser/exception/GlobalExceptionHandler.java
@@ -9,24 +9,23 @@ import org.springframework.web.context.request.WebRequest;
 import java.util.Date;
 
 
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	// handling specific exception
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<?> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request){
-		ErrorDetails errorDetails = 
-				new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-	}
+    // handling specific exception
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request) {
+        ErrorDetails errorDetails =
+                new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
 
-	// handling global exception
-	
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> globalExceptionHandling(Exception exception, WebRequest request){
-		ErrorDetails errorDetails = 
-				new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    // handling global exception
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globalExceptionHandling(Exception exception, WebRequest request) {
+        ErrorDetails errorDetails =
+                new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
